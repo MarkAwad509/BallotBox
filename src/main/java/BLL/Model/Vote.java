1fac45e1d0a3ab17c6e11faa42cc284d4f25cc53
DaydreamerBECZ <99739863+DaydreamerBECZ@users.noreply.github.com>
2022-03-16 17:19:05 -0400

    vote controller done

--- src/main/java/BLL/Model/Vote.java
+++ src/main/java/BLL/Model/Vote.java
@@ -5,12 +5,14 @@ import java.time.LocalDate;
 public class Vote{
     private static int AUTO_ID = 0;
     private int id;
+
     private LocalDate dateOfVote;
     private int rank;
-    private Candidate candidate;
+    private int candidateId; //From relation between Candidate and vote
 
-    public Vote(LocalDate when, int rank){
+    public Vote(int candidateId, LocalDate when, int rank){
         this.id = AUTO_ID;
+        this.candidateId=candidateId;
         this.dateOfVote = when;
         this.rank = rank;
         AUTO_ID++;
--- src/main/java/FEL/main.java
+++ src/main/java/FEL/main.java
@@ -1,14 +1,12 @@
 package FEL;
 
 import DAL.InMemoryRepo;
-import java.util.*;
 
 public class main {
     public static void main(String[] args) {
         InMemoryRepo Repository = InMemoryRepo.getInstance();
         if(Repository != null)
             System.out.println("all-good!");
-        //int [] r = new int[3];
-
+        //int [] r = new int[3]
     }
 }
